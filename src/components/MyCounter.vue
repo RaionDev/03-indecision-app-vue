c8669cef4acf17b3eac4ef9a3bc827b0a14e0e85
leokeeper <leonardop.keeper@gmail.com>
2024-11-26 22:10:12 -0500

    first commit

---
+++ src/components/MyCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button class="btn" @click="counter++">+1</button>
+      <button class="btn" @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { useCounter } from '@/composables/useCounter';
+
+interface Props {
+  value: number;
+}
+const props = defineProps<Props>();
+
+const { counter, squareCounter } = useCounter(props.value)
+
+
+
+
+
+</script>
+
+<style scoped>
+.btn {
+  @apply p-5 bg-blue-500 rounded mr-2 hover:bg-blue-700;
+}
+</style>
